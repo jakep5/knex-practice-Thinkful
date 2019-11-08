fe7396853ff4478082abb6d074af25b0c4e6c986
jakep5 <jakepagel1@yahoo.com>
2019-11-08 13:07:26 -0600

    add first knex instance

--- src/practice.js
+++ src/practice.js
@@ -5,3 +5,10 @@ const knexInstance = knex({
     client: 'pg',
     connection: process.env.DB_URL
 })
+
+console.log('knex and driver installed correctly');
+
+knexInstance.from('amazong_products').select('*')
+    .then(result => {
+        console.log(result)
+    })
