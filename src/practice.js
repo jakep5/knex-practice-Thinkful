f30add14cc64024aa07ccdf87442e8d67bff93a8
jakep5 <jakepagel1@yahoo.com>
2019-11-08 13:58:52 -0600

    add paginateProducts function

--- src/practice.js
+++ src/practice.js
@@ -8,7 +8,48 @@ const knexInstance = knex({
 
 console.log('knex and driver installed correctly');
 
-knexInstance.from('amazong_products').select('*') //is accessing the database here
+/* knexInstance.from('amazong_products').select('*') //is accessing the database here
     .then(result => {
         console.log(result)
-    })
+    }) */
+
+const qry = knexInstance
+    .select('product_id', 'name', 'price', 'category')
+    .from('amazong_products')
+    .where({ name: 'Point of view gun' })
+    .first()
+    .toQuery()
+    /* .then(result => {
+        console.log(result) 
+    }) */
+
+const searchTerm = 'holo';
+
+function searchByProduceName(searchTerm) {
+    knexInstance
+        .select('product_id', 'name', 'price', 'category')
+        .from('amazong_products')
+        .where('name', 'ILIKE', `%${searchTerm}%`)
+        .then(result => {
+            console.log(result)
+        })
+}
+
+searchByProduceName('holo');
+
+function paginateProducts(page) {
+    const productsPerPage = 10
+    const offset = productsPerPage * (page - 1)
+    knexInstance
+        .select('product_id', 'name', 'category')
+        .from('amazong_products')
+        .limit(productsPerPage)
+        .offset(offset)
+        .then(result => {
+            console.log(result)
+        })
+}
+
+paginateProducts(2)
+
+console.log(qry)
