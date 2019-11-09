02cee6b5d2696bbcec47bc4c66581d639541a9f5
jakep5 <jakepagel1@yahoo.com>
2019-11-09 12:58:36 -0600

    add articles-service service object

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,12 @@
+INSERT INTO blogful_articles (title, date_published, content)
+VALUES
+    ('Dummy One', now(), 'This is a test article'),
+    ('Dummy Two', now(), 'This is a test article'),
+    ('Dummy Three', now(), 'This is a test article'),
+    ('Dummy Four', now(), 'This is a test article'),
+    ('Dummy Five', now(), 'This is a test article'),
+    ('Dummy Six', now(), 'This is a test article'),
+    ('Dummy Seven', now(), 'This is a test article'),
+    ('Dummy Eight', now(), 'This is a test article'),
+    ('Dummy Nine', now(), 'This is a test article'),
+    ('Dummy Ten', now(), 'This is a test article');
